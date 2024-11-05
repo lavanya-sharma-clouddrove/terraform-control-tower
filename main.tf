5252a8f231fa092b655760674a09587559b93c10
Lavanya Sharma <lavanya.sharma@clouddrove.com>
2024-11-05 21:27:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,19 +17,18 @@ module "labels" {
 
 # Access Analyzer
 resource "aws_cloudformation_stack_set" "access_analyzer_stackset" {
-  count                   = var.enable_access_analyzer ? 1 : 0 
+  count                   = var.enable_access_analyzer ? 1 : 0
   name                    = var.access_analyser_stack_name
-  template_url            = var.access_analyzer_template_file_s3_Object_url
+  template_url            = var.access_analyzer_template_file
   administration_role_arn = var.administration_role_arn
   execution_role_name     = var.execution_role_name
 
   parameters = {
-    OrganizationId               = data.aws_organizations_organization.organization.id
+    OrganizationId                = data.aws_organizations_organization.organization.id
     AccessAnalyserMasterAccountId = var.delegated_account_id
-    ExcludedAccounts             =join(",", var.excluded_accounts) 
-    S3SourceBucket               = var.template_bucket_name
-    S3Key                        = var.access_analyzer_lambda_file_s3_key
-    RoleToAssume                 = var.role_to_assume
+    ExcludedAccounts              = var.excluded_accounts
+    S3SourceBucket                = var.template_bucket_name
+    S3Key                         = var.access_analyzer_lambda_file
   }
 
   capabilities = var.capabilities
@@ -37,17 +36,17 @@ resource "aws_cloudformation_stack_set" "access_analyzer_stackset" {
 }
 
 resource "aws_cloudformation_stack_set_instance" "access_analyzer_instance" {
-  count          = var.enable_access_analyzer ? 1 : 0 
+  count          = var.enable_access_analyzer ? 1 : 0
   account_id     = data.aws_caller_identity.current.account_id
   region         = var.region
-  stack_set_name = aws_cloudformation_stack_set.access_analyzer_stackset[0].name
+  stack_set_name = var.enable_access_analyzer ? aws_cloudformation_stack_set.access_analyzer_stackset[0].name : null
 }
 
-# Detective
+# Detective (nothing mention in docs stack or stackset)
 resource "aws_cloudformation_stack_set" "detective_stackset" {
-  count                   = var.enable_detective ? 1 : 0 
+  count                   = var.enable_detective ? 1 : 0
   name                    = var.detective_stack_name
-  template_url            = var.detective_template_file_s3_object_url
+  template_url            = var.detective_template_file
   administration_role_arn = var.administration_role_arn
   execution_role_name     = var.execution_role_name
 
@@ -55,9 +54,9 @@ resource "aws_cloudformation_stack_set" "detective_stackset" {
     OrganizationId           = data.aws_organizations_organization.organization.id
     DetectiveMasterAccountId = var.delegated_account_id
     S3SourceBucket           = var.template_bucket_name
-    S3Key                    = var.detective_lambda_file_s3_key
+    S3Key                    = var.detective_lambda_file
     RoleToAssume             = var.role_to_assume
-    ExcludedAccounts             =join(",", var.excluded_accounts) 
+    ExcludedAccounts         = var.excluded_accounts
   }
 
   capabilities = var.capabilities
@@ -65,23 +64,23 @@ resource "aws_cloudformation_stack_set" "detective_stackset" {
 }
 
 resource "aws_cloudformation_stack_set_instance" "detective_instance" {
-  count          = var.enable_detective ? 1 : 0 
+  count          = var.enable_detective ? 1 : 0
   account_id     = data.aws_caller_identity.current.account_id
   region         = var.region
-  stack_set_name = aws_cloudformation_stack_set.detective_stackset[0].name
+  stack_set_name = var.enable_detective ? aws_cloudformation_stack_set.detective_stackset[0].name : null
 }
 
-# GuardDuty
+# GuardDuty 
 resource "aws_cloudformation_stack" "guardduty_stack" {
-  count                   = var.enable_guardduty ? 1 : 0 
-  name                    = var.guardduty_stack_name
-  template_url            = var.guardduty_template_file_s3_object_url
-
+  count        = var.enable_guardduty ? 1 : 0
+  name         = var.guardduty_stack_name
+  template_url = var.guardduty_template_file
   parameters = {
-    OrganizationId      = data.aws_organizations_organization.organization.id
-    SecurityAccountId   = var.delegated_account_id
-    S3SourceBucket      = var.template_bucket_name
-    S3SourceFile        = var.guardduty_lambda_file_s3_key
+    OrganizationId    = data.aws_organizations_organization.organization.id
+    SecurityAccountId = var.delegated_account_id
+    S3SourceBucket    = var.template_bucket_name
+    S3SourceFile      = var.guardduty_s3_source_file
+    ExcludedAccounts  = var.excluded_accounts
   }
 
   capabilities = var.capabilities
@@ -92,17 +91,17 @@ resource "aws_cloudformation_stack" "guardduty_stack" {
 resource "aws_cloudformation_stack_set" "inspector_stackset" {
   count                   = var.enable_inspector ? 1 : 0
   name                    = var.inspector_stack_name
-  template_url            = var.inspector_template_file_s3_object_url
+  template_url            = var.inspector_template_file
   administration_role_arn = var.administration_role_arn
   execution_role_name     = var.execution_role_name
-  
+
   parameters = {
-    OrganizationId         = data.aws_organizations_organization.organization.id
+    OrganizationId          = data.aws_organizations_organization.organization.id
     InspectorAuditAccountId = var.delegated_account_id
-    S3SourceBucket         = var.template_bucket_name
-    S3Key                  = var.inspector_lambda_file_s3_key   
-    RoleToAssume           = var.role_to_assume
-    ExcludedAccounts             =join(",", var.excluded_accounts)
+    S3SourceBucket          = var.template_bucket_name
+    S3Key                   = var.inspector_lambda_file
+    RoleToAssume            = var.role_to_assume
+    ExcludedAccounts        = var.excluded_accounts
   }
 
   capabilities = var.capabilities
@@ -113,27 +112,30 @@ resource "aws_cloudformation_stack_set_instance" "inspector_instance" {
   count          = var.enable_inspector ? 1 : 0
   account_id     = data.aws_caller_identity.current.account_id
   region         = var.region
-  stack_set_name = aws_cloudformation_stack_set.inspector_stackset[0].name
+  stack_set_name = var.enable_inspector ? aws_cloudformation_stack_set.inspector_stackset[0].name : null
 }
 
 # Security Hub
 resource "aws_cloudformation_stack" "security_hub_stack" {
-  count                   = var.enable_security_hub ? 1 : 0
-  name                    = var.security_hub_stack_name
-  template_url            = var.security_hub_template_file_s3_object_url
+  count        = var.enable_security_hub ? 1 : 0
+  name         = var.security_hub_stack_name
+  template_url = var.security_hub_template_file
 
   parameters = {
-    SecurityAccountId   = var.delegated_account_id
-    ExcludedAccounts    = join(",", var.excluded_accounts)
-    OrganizationId      = data.aws_organizations_organization.organization.id
-    S3SourceBucket      = var.template_bucket_name
-    S3SourceKey         = var.security_hub_lambda_file_s3_key
-    RoleToAssume        = var.role_to_assume
-    AWSStandard         = var.security_hub_aws_standard
-    CIS120Standard      = var.security_hub_cis120_standard
-    CIS140Standard      = var.security_hub_cis140_standard
-    PCIStandard         = var.security_hub_pci_standard
-    NISTStandard        = var.security_hub_nist_standard
+    SecurityAccountId = var.delegated_account_id
+    ExcludedAccounts  = var.excluded_accounts
+    OrganizationId    = data.aws_organizations_organization.organization.id
+    # RegionFilter        = var.security_hub_region_filter
+    # OUFilter            = var.security_hub_ou_filter
+    S3SourceBucket = var.template_bucket_name
+    S3SourceKey    = var.security_hub_s3_source_key
+    # ComplianceFrequency = var.security_hub_compliance_frequency
+    RoleToAssume   = var.role_to_assume
+    AWSStandard    = var.security_hub_aws_standard
+    CIS120Standard = var.security_hub_cis120_standard
+    CIS140Standard = var.security_hub_cis140_standard
+    PCIStandard    = var.security_hub_pci_standard
+    NISTStandard   = var.security_hub_nist_standard
   }
 
   capabilities = var.capabilities
@@ -142,15 +144,13 @@ resource "aws_cloudformation_stack" "security_hub_stack" {
 
 # Config
 resource "aws_cloudformation_stack" "config_stack" {
-  count                   = var.enable_config ? 1 : 0
-  name                    = var.config_stack_name
-  template_url            = var.config_template_file_s3_object_url
+  count        = var.enable_config ? 1 : 0
+  name         = var.config_stack_name
+  template_url = var.config_template_url
 
   parameters = {
-    ExcludedAccounts             =join(",", var.excluded_accounts)
-    ConfigRecorderExcludedResourceTypes = var.ConfigRecorderExcludedResourceTypes
-    CloudFormationVersion              = var.cloudformation_version
-    S3SourceBucket                     = var.template_bucket_name
+    ExcludedAccounts = var.excluded_accounts
+    S3SourceBucket   = var.template_bucket_name
   }
 
   capabilities = var.capabilities
@@ -159,16 +159,16 @@ resource "aws_cloudformation_stack" "config_stack" {
 
 # Inspection Lambda
 resource "aws_cloudformation_stack" "inspection_lambda_stack" {
-  count                   = var.enable_inspection_lambda ? 1 : 0
-  name                    = var.inspection_lambda_stack_name
-  template_url            = var.inspection_lambda_template_s3_object_url
+  count        = var.enable_inspection_lambda ? 1 : 0
+  name         = var.inspection_stack_name
+  template_url = var.inspection_lambda_template_file
 
   parameters = {
-    DestinationBucketName = var.inspection_destination_bucket_name  
-    OrganizationId        = data.aws_organizations_organization.organization.id          
+    DestinationBucketName = var.inspection_destination_bucket_name
+    OrganizationId        = data.aws_organizations_organization.organization.id
     AssumeRole            = var.role_to_assume
     S3SourceBucket        = var.template_bucket_name
-    S3Key                 = var.inspection_lambda_lambda_file_s3_key 
+    S3Key                 = var.inspection_lambda_key
   }
 
   capabilities = var.capabilities
@@ -177,16 +177,17 @@ resource "aws_cloudformation_stack" "inspection_lambda_stack" {
 
 # Macie
 resource "aws_cloudformation_stack" "macie_stack" {
-  count                   = var.enable_macie ? 1 : 0
-  name                    = var.macie_stack_name
-  template_url            = var.macie_template_file_object_url
+  count        = var.enable_macie ? 1 : 0
+  name         = var.macie_stack_name
+  template_url = var.macie_template_file
 
   parameters = {
-    OrganizationId        = data.aws_organizations_organization.organization.id          
-    AssumeRole            = var.role_to_assume
-    S3SourceBucket        = var.template_bucket_name
-    S3Key                 = var.macie_lambda_file_s3_key
+    OrganizationId = data.aws_organizations_organization.organization.id
+    # RoleToAssume          = var.role_to_assume
+    S3SourceBucket       = var.template_bucket_name
+    S3Key                = var.macie_lambda_file
     MacieMasterAccountId = var.delegated_account_id
+    ExcludedAccounts     = var.excluded_accounts
   }
 
   capabilities = var.capabilities
@@ -195,15 +196,15 @@ resource "aws_cloudformation_stack" "macie_stack" {
 
 # Subdomain Delegation (Master Stack)
 resource "aws_cloudformation_stack" "master_stack" {
-  count                   = var.enable_subdomain_delegation_master ? 1 : 0
-  name                    = var.subdomain_delegation_master_stack_name
-  template_url            = var.subdomain_delegation_master_template_file_s3_object_url
+  count        = var.enable_subdomain_delegation_master ? 1 : 0
+  name         = var.subdomain_delegation_master_stack_name
+  template_url = var.subdomain_delegation_template_file
 
   parameters = {
-    HostedZoneId       = var.subdomain_delegation_hosted_zone_id  
-    AuthorizedAccounts = var.subdomain_delegation_authorized_account 
-    S3Bucket           = var.template_bucket_name  
-    S3Key              = var.subdomain_delegation_lambda_file_s3_key
+    HostedZoneId       = var.subdomain_delegation_hosted_zone_id
+    AuthorizedAccounts = var.subdomain_delegation_authorized_account
+    S3Bucket           = var.template_bucket_name
+    S3Key              = var.subdomain_delegation_s3_key
   }
 
   capabilities = var.capabilities
@@ -212,32 +213,141 @@ resource "aws_cloudformation_stack" "master_stack" {
 
 # Subdomain Delegation (Child Stack)
 resource "aws_cloudformation_stack" "child_stack" {
-  count                   = var.enable_subdomain_delegation_child ? 1 : 0
-  name                    = var.subdomain_delegation_child_stack_name
-  template_url            = var.subdomain_delegation_child_template_file_s3_object_url
+  count        = var.enable_subdomain_delegation_child ? 1 : 0
+  name         = var.subdomain_delegation_child_stack_name
+  template_url = var.subdomain_delegation_template_file
 
   parameters = {
-    DomainName      = var.subdomain_delegation_domain_name 
-    MasterAccountId = var.delegated_account_id  
+    DomainName      = var.subdomain_delegation_domain_name
+    MasterAccountId = var.subdomain_delegation_master_account_id
+  }
+
+  capabilities = var.capabilities
+  tags         = module.labels.tags
+}
+#aws webhook
+resource "aws_cloudformation_stack" "ct_notifications_stack" {
+  count        = var.enable_notification_webhook ? 1 : 0
+  name         = var.notification_webhook_stack_name
+  template_url = var.notification_webhook_template_file
+
+  parameters = {
+    WebhookUrl   = var.webhook_url
+    RuleFilter   = var.notification_webhook_rule_filter
+    S3BucketName = var.template_bucket_name
+    S3Key        = var.notification_webhook_s3_key
   }
 
   capabilities = var.capabilities
   tags         = module.labels.tags
 }
 
-# AWS Webhook
-resource "aws_cloudformation_stack" "ct_notifications_stack" {
-  count         = var.enable_notification_webhook ? 1 : 0
-  name          = var.notification_webhook_stack_name
-  template_url  = var.notification_webhook_template_file_s3_object_url
+# AWS Backups
+resource "aws_cloudformation_stack_set" "backup_member_account_role" {
+  count                   = var.enable_backup_member_account_role ? 1 : 0
+  name                    = "awsbackup-member-accounts-role"
+  template_url            = var.backup_member_role_template_file
+  administration_role_arn = var.administration_role_arn
+  execution_role_name     = var.execution_role_name
 
   parameters = {
-    WebhookUrl      = var.webhook_url  
-    RuleFilter      = var.notification_webhook_rule_filter
-    S3BucketName        = var.template_bucket_name  
-    S3Key           = var.notification_webhook_lambda_file_s3_key 
+    pCrossAccountBackupRole = var.pCrossAccountBackupRole
+    pTagKey1                = var.pTagKey1
+    pTagValue1              = var.pTagValue1
   }
 
   capabilities = var.capabilities
   tags         = module.labels.tags
+}
+
+resource "aws_cloudformation_stack_set_instance" "backup_member_account_role_instance" {
+  count          = var.enable_backup_member_account_role ? 1 : 0
+  account_id     = data.aws_caller_identity.current.account_id
+  region         = var.region
+  stack_set_name = var.enable_backup_member_account_role ? aws_cloudformation_stack_set.backup_member_account_role[0].name : null
+}
+
+resource "aws_cloudformation_stack_set" "backup_member_account" {
+  count                   = var.enable_backup_member_account ? 1 : 0
+  name                    = "backup_member_account"
+  template_url            = var.backup_member_account_file
+  administration_role_arn = var.administration_role_arn
+  execution_role_name     = var.execution_role_name
+
+  parameters = {
+    pTagKey1                = var.pTagKey1
+    pTagValue1              = var.pTagValue1
+    pCrossAccountBackupRole = var.pCrossAccountBackupRole
+    pBackupKeyAlias         = var.pBackupKeyAlias
+    pMemberBackupVaultName  = var.pMemberBackupVaultName
+    pOrganizationId         = data.aws_organizations_organization.organization.id
+  }
+
+  capabilities = var.capabilities
+  tags         = module.labels.tags
+}
+
+resource "aws_cloudformation_stack_set_instance" "backup_member_account_instance" {
+  count          = var.enable_backup_member_account ? 1 : 0
+  account_id     = data.aws_caller_identity.current.account_id
+  region         = var.region
+  stack_set_name = var.enable_inspector ? aws_cloudformation_stack_set.backup_member_account[0].name : null
+}
+
+resource "aws_cloudformation_stack_set" "central_backup_account" {
+  count                   = var.enable_central_backup_account ? 1 : 0
+  name                    = "backup_member_account"
+  template_url            = var.central_backup_account_file
+  administration_role_arn = var.administration_role_arn
+  execution_role_name     = var.execution_role_name
+
+  parameters = {
+    pTagKey1                = var.pTagKey1
+    pTagValue1              = var.pTagValue1
+    pBackupKeyAlias         = var.pBackupKeyAlias
+    pCrossAccountBackupRole = var.pCrossAccountBackupRole
+    pCentralBackupVaultName = var.pMemberBackupVaultName
+    pOrganizationId         = data.aws_organizations_organization.organization.id
+  }
+
+  capabilities = var.capabilities
+  tags         = module.labels.tags
+}
+
+resource "aws_cloudformation_stack_set_instance" "backup_member_account_instance" {
+  count          = var.enable_central_backup_account ? 1 : 0
+  account_id     = data.aws_caller_identity.current.account_id
+  region         = var.region
+  stack_set_name = var.enable_inspector ? aws_cloudformation_stack_set.central_backup_account[0].name : null
+}
+
+resource "aws_cloudformation_stack_set" "central_backup_org_account" {
+  count                   = var.enable_central_backup_org_account ? 1 : 0
+  name                    = var.inspector_stack_name
+  template_url            = var.central_backup_org_account_template_file
+  administration_role_arn = var.administration_role_arn
+  execution_role_name     = var.execution_role_name
+
+  parameters = {
+    pCrossAccountBackupRole   = var.pCrossAccountBackupRole
+    pTagKey                   = var.pTagKey1
+    pTagValue                 = var.pTagValue1
+    pOrgbackupAccounts        = data.aws_caller_identity.current.account_id
+    pMemberAccountBackupVault = var.pMemberBackupVaultName
+    pCentralBackupVaultArn    = var.pCentralBackupVaultArn # arn of awscentralbackupvault in security account 
+    pBackupTagKey1            = var.pTagKey1
+    pBackupTagKey2            = var.pBackupTagKey2
+    pBackupTagValue1          = var.pTagValue1
+    pBackupTagValue2          = var.pBackupTagValue2
+  }
+
+  capabilities = var.capabilities
+  tags         = module.labels.tags
+}
+
+resource "aws_cloudformation_stack_set_instance" "central_backup_org_account_instance" {
+  count          = var.enable_central_backup_org_account ? 1 : 0
+  account_id     = data.aws_caller_identity.current.account_id
+  region         = var.region
+  stack_set_name = var.enable_central_backup_org_account ? aws_cloudformation_stack_set.central_backup_org_account[0].name : null
 }
