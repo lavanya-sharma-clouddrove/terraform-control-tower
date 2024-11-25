3df90d9d0583306076e44847e899361c85419703
Lavanya Sharma <lavanya.sharma@clouddrove.com>
2024-11-25 22:00:42 +0530

    Update variables.tf

--- prerequsite/variables.tf
+++ prerequsite/variables.tf
@@ -1,4 +1,12 @@
-
-
-
-
+variable "name" {
+  type    = string
+  default = "nov-25"
+}
+variable "environment" {
+  type    = string
+  default = "test"
+}
+variable "label_order" {
+  type    = list(string)
+  default = ["name", "environment"]
+}
